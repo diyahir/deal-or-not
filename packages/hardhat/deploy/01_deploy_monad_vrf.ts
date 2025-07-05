7287d0770380fe6d7ba37a3ed8428f4b20b22831
meirbank <meir@meirbank.com>
2025-07-05 23:09:12 +0200

    tests for monad vrf

--- packages/hardhat/deploy/01_deploy_monad_vrf.ts
+++ packages/hardhat/deploy/01_deploy_monad_vrf.ts
@@ -9,15 +9,15 @@ import { Contract } from "ethers";
  */
 const deployMonadVRF: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
-      On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
-  
-      When deploying to live networks (e.g `yarn deploy --network monadTestnet`), the deployer account
-      should have sufficient balance to pay for the gas fees for contract creation.
-  
-      You can generate a random account with `yarn generate` or `yarn account:import` to import your
-      existing PK which will fill DEPLOYER_PRIVATE_KEY_ENCRYPTED in the .env file (then used on hardhat.config.ts)
-      You can run the `yarn account` command to check your balance in every network.
-    */
+        On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
+    
+        When deploying to live networks (e.g `yarn deploy --network monadTestnet`), the deployer account
+        should have sufficient balance to pay for the gas fees for contract creation.
+    
+        You can generate a random account with `yarn generate` or `yarn account:import` to import your
+        existing PK which will fill DEPLOYER_PRIVATE_KEY_ENCRYPTED in the .env file (then used on hardhat.config.ts)
+        You can run the `yarn account` command to check your balance in every network.
+      */
   // Use custom signer from MONAD_PK environment variable
   const privateKey = process.env.MONAD_PK;
   if (!privateKey) {

bdcc566f9f41c85141840d2b91d46751b7431c23
meirbank <meir@meirbank.com>
2025-07-06 13:12:15 +0200

    add lizard

--- packages/nextjs/src/components/BankOffer.tsx
+++ packages/nextjs/src/components/BankOffer.tsx
@@ -12,6 +12,8 @@ import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { formatEther } from 'viem';
 import { useAccount, usePublicClient, useReadContract, useWriteContract } from 'wagmi';
+import Image from 'next/image';
+import Lizza from '@/assets/lizza.png';
 
 export function BankOffer() {
   const client = usePublicClient();
@@ -112,56 +114,59 @@ export function BankOffer() {
   };
 
   return (
-    <div
-      className={cn(
-        'border mx-auto border-[#f86e02] rounded-xl text-white bg-[#01152C] p-4 flex flex-col justify-center gap-6',
-        'items-center w-[625px] h-[200px]'
-      )}
-    >
+    <div className="flex items-center justify-center">
       <div
         className={cn(
-          'flex items-center w-fit mx-auto justify-between bg-[#1b4061] rounded-full',
-          !game.canAccept && 'opacity-50'
+          'border mx-auto border-[#f86e02] rounded-xl text-white bg-[#01152C] p-4 flex flex-col justify-center gap-6',
+          'items-center w-[625px] h-[200px]'
         )}
       >
-        <span className="text-2xl font-semibold bg-[#f86e02] rounded-full p-4">BANK OFFER:</span>
-        <span className="text-[#F86E00] text-2xl font-semibold pl-12 pr-4">
-          {game.canAccept ? `${Number(formatEther((offer as bigint) || 0n)).toFixed(5)} gMON` : 'no offers yet'}
-        </span>
-      </div>
-
-      {(gameState as { isActive?: boolean })?.isActive ? (
-        <button
-          onClick={acceptDeal}
+        <div
           className={cn(
-            'bg-[#3fa43e] text-2xl font-semibold rounded-lg py-4 px-8 min-w-[155px] flex justify-center',
-            !game.canAccept && 'cursor-not-allowed opacity-50 hover:opacity-50'
+            'flex items-center w-fit mx-auto justify-between bg-[#1b4061] rounded-full',
+            !game.canAccept && 'opacity-50'
           )}
-          disabled={!game.canAccept}
         >
-          {isLoadingAccept ? <Loading /> : 'ACCEPT'}
-        </button>
-      ) : (
-        <ConnectButton.Custom>
-          {({ account, openConnectModal, mounted }) => {
-            const isConnected = mounted && account;
+          <span className="text-2xl font-semibold bg-[#f86e02] rounded-full p-4">BANK OFFER:</span>
+          <span className="text-[#F86E00] text-2xl font-semibold pl-12 pr-4">
+            {game.canAccept ? `${Number(formatEther((offer as bigint) || 0n)).toFixed(5)} gMON` : 'no offers yet'}
+          </span>
+        </div>
 
-            if (isConnected && !chain) {
-              return <SwitchNetwork />;
-            }
+        {(gameState as { isActive?: boolean })?.isActive ? (
+          <button
+            onClick={acceptDeal}
+            className={cn(
+              'bg-[#3fa43e] text-2xl font-semibold rounded-lg py-4 px-8 min-w-[155px] flex justify-center',
+              !game.canAccept && 'cursor-not-allowed opacity-50 hover:opacity-50'
+            )}
+            disabled={!game.canAccept}
+          >
+            {isLoadingAccept ? <Loading /> : 'ACCEPT'}
+          </button>
+        ) : (
+          <ConnectButton.Custom>
+            {({ account, openConnectModal, mounted }) => {
+              const isConnected = mounted && account;
 
-            return (
-              <button
-                className="bg-[#F86E00] text-white text-2xl font-semibold rounded-lg py-4 px-8 min-w-[190px] flex justify-center"
-                onClick={!isConnected ? openConnectModal : startGame}
-                disabled={isLoading}
-              >
-                {isLoading ? <Loading /> : !isConnected ? 'Connect Wallet' : 'Start Game'}
-              </button>
-            );
-          }}
-        </ConnectButton.Custom>
-      )}
+              if (isConnected && !chain) {
+                return <SwitchNetwork />;
+              }
+
+              return (
+                <button
+                  className="bg-[#F86E00] text-white text-2xl font-semibold rounded-lg py-4 px-8 min-w-[190px] flex justify-center"
+                  onClick={!isConnected ? openConnectModal : startGame}
+                  disabled={isLoading}
+                >
+                  {isLoading ? <Loading /> : !isConnected ? 'Connect Wallet' : 'Start Game'}
+                </button>
+              );
+            }}
+          </ConnectButton.Custom>
+        )}
+      </div>
+      <Image src={Lizza} alt="lizza" className="h-[200px] w-auto ml-4 object-contain" />
     </div>
   );
 }
